b251f4432b795c89fd03e501de13b3703b189ccb
kun-li <45413597+likun-k@users.noreply.github.com>
2020-12-02 09:57:21 +0800

    add memento

---
+++ src/main/java/com/kun/behavior/memento/Memento.java
@@ -0,0 +1,15 @@
+package com.kun.behavior.memento;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return state;
+    }
+
+}
